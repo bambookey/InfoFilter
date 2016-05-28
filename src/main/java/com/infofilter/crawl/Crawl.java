971c07f9fc59b4f877ec2ca3be0472a5424e6d0f
bambookey <redleaf00@126.com>
2016-05-29 03:01:44 +0800

    basic crawl functions

---
+++ src/main/java/com/infofilter/crawl/Crawl.java
@@ -0,0 +1,61 @@
+package com.infofilter.crawl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Selectable;
+
+public class Crawl implements PageProcessor {
+	public static final String URL_LIST = "https://www.douban.com/group/zhufang/discussion";
+
+    //public static final String URL_POST = "http://auto.163.com/blog/";
+
+    private Site site = Site
+            .me()
+            .setDomain("auto.163.com")	
+            .setSleepTime(3000)
+            .setUserAgent(
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
+
+    public void process(Page page) {
+        //列表页
+        if (page.getUrl().regex(URL_LIST).match()) {
+        	System.out.println();
+        	List<Selectable> links = page.getHtml().xpath("//td[@class='title']").nodes();
+        	String title = "";
+        	String link = "";
+        	for(Selectable s : links) {
+        		title = s.xpath("//a/@title").toString();
+        		link = s.xpath("//a/@href").toString();
+        		if(title.contains("海淀")) {
+        			System.out.println(title+":"+link);
+        		}
+        	}
+        	
+            page.addTargetRequests(page.getHtml().xpath("//span[@class='next']").links().all());
+            return;
+        //文章页
+        } else {
+            page.putField("title", page.getHtml().xpath("//h1[@id='h1title']/text()"));
+            page.putField("source", page.getHtml().xpath("//a[@id='ne_article_source']/text()"));
+        }
+    }
+
+    public Site getSite() {
+        return site;
+    }
+
+    public void run(String startUrl) {
+    	Spider.create(new Crawl()).addUrl(startUrl).run();
+    }
+    
+    public static void main(String[] args) {
+    	Crawl c = new Crawl();
+    	String startUrl = "https://www.douban.com/group/zhufang/discussion?start=0";
+        c.run(startUrl);
+    }
+}
